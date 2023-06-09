5ec307fe76f1f3d5d533db6b1677c0f4b1064fcf
Ivan123321789 <vetluga11@yandex.ru>
2023-06-09 13:37:58 +0300

    исправление ошибок тестирования

--- src/models/user.js
+++ src/models/user.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema(
       required: [true, 'Поле "name" должно быть заполнено'],
       minlength: [2, 'Минимальная длина поля "name" - 2'],
       maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    },  
+    },
     about: {
       type: String,
       required: [true, 'Поле "about" должно быть заполнено'],
@@ -22,8 +22,8 @@ const userSchema = new mongoose.Schema(
         message: 'Некорректный URL',
       },
       required: true,
-    },    
+    },
   },
-  { versionKey: false }
+  { versionKey: false },
 );
 module.exports = mongoose.model('user', userSchema);
