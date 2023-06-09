c62d4cc0d5a3b4772c0381ada8ee22c4ee1c4ea9
Ivan123321789 <vetluga11@yandex.ru>
2023-06-09 13:35:04 +0300

    исправление ошибок тестирования

--- src/models/card.js
+++ src/models/card.js
@@ -4,38 +4,36 @@ const isURL = require('validator/lib/isURL');
 
 const cardSchema = new mongoose.Schema(
   {
-  name: {
-    type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" - 2'],
-    maxlength: [30, 'Максимальная длина поля "name" - 30'],
-  },
-  link: {
-    type: String,
-    validate: {
-      validator: (v) => isURL(v),
-      message: 'Некорректный URL',
+    name: {
+      type: String,
+      required: [true, 'Поле "name" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
     },
-    required: true,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    default: [],
-    required: true,
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
- 
-},  
-      
-{ versionKey: false });
-
+    link: {
+      type: String,
+      validate: {
+        validator: (v) => isURL(v),
+        message: 'Некорректный URL',
+      },
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    likes: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      default: [],
+      required: true,
+    }],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },  
+  { versionKey: false }
+);
 module.exports = mongoose.model('card', cardSchema);
--- src/models/user.js
+++ src/models/user.js
@@ -1,30 +1,29 @@
 const mongoose = require('mongoose');
 const isURL = require('validator/lib/isURL');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" - 2'],
-    maxlength: [30, 'Максимальная длина поля "name" - 30'],
-  },  
-  about: {
-    type: String,
-    required: [true, 'Поле "about" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "about" - 2'],
-    maxlength: [30, 'Максимальная длина поля "about" - 30'],
-  },
-
-  avatar: {
-    type: String,
-    validate: {
-      validator: (v) => isURL(v),
-      message: 'Некорректный URL',
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Поле "name" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    },  
+    about: {
+      type: String,
+      required: [true, 'Поле "about" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "about" - 2'],
+      maxlength: [30, 'Максимальная длина поля "about" - 30'],
     },
-    required: true,
+    avatar: {
+      type: String,
+      validate: {
+        validator: (v) => isURL(v),
+        message: 'Некорректный URL',
+      },
+      required: true,
+    },    
   },
-      
-},
-{ versionKey: false });
-
+  { versionKey: false }
+);
 module.exports = mongoose.model('user', userSchema);
