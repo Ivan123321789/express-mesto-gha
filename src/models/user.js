9a23b0b2fd3612faa0a192565bc99b2a5a2e993f
Ivan123321789 <vetluga11@yandex.ru>
2023-06-21 21:34:03 +0300

    как я устал

--- src/models/user.js
+++ src/models/user.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       minlength: [2, 'Минимальная длина поля "name" - 2'],
       maxlength: [30, 'Максимальная длина поля "name" - 30'],
-      default: 'Юрий Гагарин',
+      default: 'Жак-Ив Кусто',
     },
     about: {
       type: String,
