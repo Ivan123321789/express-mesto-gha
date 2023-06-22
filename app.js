4279573473ad6cec3f3646f37ac7d5f3bcbbb726
Ivan123321789 <vetluga11@yandex.ru>
2023-06-22 18:47:23 +0300

    хорошая погода

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const mongoose = require('mongoose');
-// const helmet = require('helmet');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { authValidation, regValidation } = require('./src/middlewares/validation');
 const { login, createUser } = require('./src/controllers/users');
@@ -10,10 +10,11 @@ const auth = require('./src/middlewares/auth');
 const userRouter = require('./src/routes/users');
 const cardRouter = require('./src/routes/cards');
 const NotFound = require('./src/errors/NotFound');
+const errorHandler = require('./src/middlewares/errorHandler');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
-// app.use(helmet());
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -34,22 +35,7 @@ app.use('/', (req, res, next) => {
 });
 
 app.use(errors());
-
-// app.use((err, req, res, next) => {
-//   const { statusCode = 500, message } = err;
-//   res.status(statusCode)
-//     .send({
-//       message: statusCode === 500
-//         ? 'Буря мглою небо кроет'
-//         : message,
-//     });
-//   next();
-// });
-app.use((err, req, res, next) => {
-  res.send({ message: err.message });
-  next();
-});
-
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,10 @@
+module.exports = ((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode)
+    .send({
+      message: statusCode === 500
+      ? 'Буря мглою небо кроет'
+      : message,
+  });
+  next();
+});
