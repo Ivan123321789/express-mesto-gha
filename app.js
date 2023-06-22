02a73efb053e77ffedfd8a03d71d39115a9c3e16
Ivan123321789 <vetluga11@yandex.ru>
2023-06-22 18:34:09 +0300

    хорошая погода

--- app.js
+++ app.js
@@ -35,14 +35,18 @@ app.use('/', (req, res, next) => {
 
 app.use(errors());
 
+// app.use((err, req, res, next) => {
+//   const { statusCode = 500, message } = err;
+//   res.status(statusCode)
+//     .send({
+//       message: statusCode === 500
+//         ? 'Буря мглою небо кроет'
+//         : message,
+//     });
+//   next();
+// });
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'Буря мглою небо кроет'
-        : message,
-    });
+  res.send({ message: err.message });
   next();
 });
 
